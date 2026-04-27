78e72bcbe5da82ad1c28d4bbccc964bd3468a94d
TheJoelOM <jeymelon@gmail.com>
2026-04-27 16:53:26 +0200

    added to models

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,4 +1,5 @@
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace data2410_api_v1.Models;
 
@@ -9,4 +10,16 @@ public class Student
     public string Course { get; set; }
     public int Marks { get; set; }
     public string? Grade { get; set; }
+}
+
+public class CourseReport
+{
+    public string Course {get; set;} = string.Empty;
+    public int TotalStudents {get; set;}
+    public double AverageMarks {get; set;}
+    public int ACount { get; set;}
+    public int BCount {get; set;}
+    public int CCount {get; set;}
+
+    public int DCount{get; set;}
 }
